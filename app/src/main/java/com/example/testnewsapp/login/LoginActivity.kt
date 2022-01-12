75e40bca8c50846fc8734499b807c4096d490de7
seropp <ts.seropps@gmail.com>
2022-01-12 17:17:17 +0300

    Fixed user authorization. Implemented adding users to the database

--- app/src/main/java/com/example/testnewsapp/login/LoginActivity.kt
+++ app/src/main/java/com/example/testnewsapp/login/LoginActivity.kt
@@ -1,91 +1,87 @@
 package com.example.testnewsapp.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import com.example.testnewsapp.R
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.testnewsapp.MainActivity
+import com.example.testnewsapp.R
+import com.example.testnewsapp.databinding.ActivityLoginBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class LoginActivity : AppCompatActivity() {
 
-
-    private val rqCode:Int =  1710
-    private lateinit var providers: List<AuthUI.IdpConfig>
+    private lateinit var binding: ActivityLoginBinding
+    private val auth = FirebaseAuth.getInstance().currentUser
     private lateinit var btnSignOut: Button
+    private lateinit var btnLogIn: Button
+    private lateinit var btnBeginTo: Button
+    private lateinit var users: DatabaseReference
+    private lateinit var currentUserReference: DatabaseReference
 
+
+    companion object {
+        var idCurrentUser: String? = null
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
-        btnSignOut = findViewById(R.id.btn_sing_out)
-        btnSignOut.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(p0: View?) {
-                AuthUI.getInstance()
-                    .signOut(this@LoginActivity)
-                    .addOnCanceledListener {object : OnCompleteListener<Unit> {
-                        override fun onComplete(p0: Task<Unit>) {
-                            btnSignOut.isEnabled = false
-                            showSingOptions()
-                        }
+        if (auth != null) {
+            createUI()
+        }
 
-                    } }.addOnFailureListener(object : OnFailureListener{
-                        override fun onFailure(p0: Exception) {
-                            Toast.makeText(this@LoginActivity, ""+p0.message.toString(), Toast.LENGTH_LONG).show()
-                        }
-
-                    })
+        btnSignOut = findViewById(R.id.btn_signout)
+        btnSignOut.setOnClickListener {
+            AuthUI.getInstance().signOut(this).addOnSuccessListener {
+                val loginTxtName: TextView = findViewById(R.id.login_txt_name)
+                val loginEmail: TextView = findViewById(R.id.login_email)
+                loginTxtName.text = "Username"
+                loginEmail.text = "Email"
+                idCurrentUser = null
+                Toast.makeText(this, "Success logged Out", Toast.LENGTH_LONG).show()
             }
+        }
 
-
-        })
-
-
-
-        providers = listOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build(),
-            AuthUI.IdpConfig.FacebookBuilder().build(),
-            AuthUI.IdpConfig.TwitterBuilder().build())
-
-        showSingOptions()
-    }
-
-    private fun showSingOptions() {
-        startActivityForResult(
-            AuthUI.getInstance().createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(), rqCode
-        )
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode ==  rqCode) {
-            val response: IdpResponse? = IdpResponse.fromResultIntent(data)
-            if(resultCode == requestCode) {
-                val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
-                Toast.makeText(this, "" + user?.email, Toast.LENGTH_LONG).show()
-                btnSignOut.isEnabled = true
-                switchAct()
-            } else Toast.makeText(this, ""+ response?.error!!.message.toString(), Toast.LENGTH_LONG).show()
+        btnLogIn = findViewById(R.id.btn_log_in)
+        btnLogIn.setOnClickListener {
+            startActivity(Intent(this, LoginFirebaseUI::class.java))
+        }
+        btnBeginTo = findViewById(R.id.btn_to_begin_login)
+        btnBeginTo.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAffinity()
+    }
 
-    private fun switchAct() {
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+    private fun createUI() {
+        auth?.let {
+
+            val loginTxtName: TextView = findViewById(R.id.login_txt_name)
+            val loginEmail: TextView = findViewById(R.id.login_email)
+            loginTxtName.text = auth.displayName
+            loginEmail.text = auth.email
+            idCurrentUser = auth.uid
+
+            users = FirebaseDatabase.getInstance().getReference("users")
+            currentUserReference = users.child(auth.uid)
+            val userMap = mapOf(
+                "email" to auth.email,
+                "name" to auth.displayName,
+            )
+            currentUserReference.updateChildren(userMap)
+        }
     }
 }
---
+++ app/src/main/java/com/example/testnewsapp/login/LoginFirebaseUI.kt
@@ -0,0 +1,67 @@
+package com.example.testnewsapp.login
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.testnewsapp.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginFirebaseUI : AppCompatActivity() {
+
+    companion object {
+        private const val RC_SIGN = 1710
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        createLoginUI()
+    }
+
+    private fun createLoginUI() {
+        val providers = arrayListOf<AuthUI.IdpConfig>(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build(),
+            AuthUI.IdpConfig.TwitterBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.Theme_Design_Light_NoActionBar)
+                .build(), RC_SIGN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN) {
+
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+
+                startActivity(Intent(this, LoginActivity::class.java))
+            } else {
+                if (response == null) {
+                    finish()
+                }
+                if (response?.error?.errorCode == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "Internet connection lost. Try later.", Toast.LENGTH_LONG)
+                        .show()
+                    return
+                } else if (response?.error?.errorCode == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, response.error?.errorCode.toString(), Toast.LENGTH_LONG)
+                        .show()
+                    return
+                }
+            }
+        }
+    }
+}
